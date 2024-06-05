32890ce50e43dffd1302076b6b4dc9e07d3c5166
Maheboob7699 <murdimaheboob@gmail.com>
2024-06-05 08:40:54 -0900

    Login

--- Login/src/Login1.jsx
+++ Login/src/Login1.jsx
@@ -1,19 +1,25 @@
+import React,{useState} from "react";
+
 
 function Login(){
-
+const [count, setcount]= useState(0);
 
 return(
 
-    <div style={{textAlign:"center"}}>
+    <div style={{textAlign:"center", backgroundColor:"blue",width:"250px",height:"350px"}}>
+      <h2> counting {count} </h2>
       <h1>Login</h1>
-      
-      <input type="text" placeholder="Enter Username here" />
+
+      <div style={{width:"100px",marginLeft:"40px"}}>
+      <input type="text" placeholder="Enter Username here" /></div>
       <br/>
       <br/>
       <input type="password" placeholder="Enter Password here" />
       <br/>
       <br/>
-      <button type="submit">login</button>
+      <button type="submit">login</button><br/><br/>
+      <button onClick={()=> setcount(count + 1)}>Increnment</button><br/><br/>
+      <button onClick={()=> setcount(count - 1)}>Decerement</button>
 
     </div>
 )
