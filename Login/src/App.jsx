ab307b5eb6b4ce3936a812d15c8f47236462494c
Maheboob7699 <murdimaheboob@gmail.com>
2024-06-04 22:03:31 -0900

    login

---
+++ Login/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import Login from './Login1'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Login></Login>
+    </>
+  )
+}
+
+export default App
---
+++ Login/src/Login1.jsx
@@ -0,0 +1,23 @@
+
+function Login(){
+
+
+return(
+
+    <div style={{textAlign:"center"}}>
+      <h1>Login</h1>
+      
+      <input type="text" placeholder="Enter Username here" />
+      <br/>
+      <br/>
+      <input type="password" placeholder="Enter Password here" />
+      <br/>
+      <br/>
+      <button type="submit">login</button>
+
+    </div>
+)
+
+}
+
+export default Login;
