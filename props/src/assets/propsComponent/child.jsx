bbcc53bcb04597a85458a879f539e5ffb9caeb0d
Maheboob7699 <murdimaheboob@gmail.com>
2024-08-18 04:29:07 -0900

    Props

---
+++ props/src/assets/propsComponent/child.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Child(props){
+    return(
+
+    <>
+    
+    <h1>{props.name}</h1>
+    <h2>{props.id}</h2>
+    </>
+
+
+
+    )
+}
+
+export default Child;
---
+++ props/src/assets/propsComponent/parent.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Child from '../propsComponent/child'
+
+
+
+function Parent(){
+    return(
+
+        <>
+             <Child
+             name="Mehboob "
+             id="134"
+             />   
+
+              <Child
+             name="Anuj "
+             id="166"
+              />     
+                   
+                  
+       </>
+    )
+}
+
+export default Parent;
